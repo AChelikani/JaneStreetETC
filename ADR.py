8c0c744ea2975d5a2c5fe119ac6d7b4f5e40a2dd
Parker Hao <phao@mit.edu>
2017-07-22 11:50:14 -0700

    ETF renamed to ADR

--- ADR.py
+++ ADR.py
@@ -2,7 +2,7 @@
 
 NOKUS_PRICE = 1000
 NOKUS_QUANT = 10
-NOKFH_PRICE = 1000
+NOKFH_PRICE = 1005
 NOKFH_QUANT = 10
 
 def ETF(NOKUS_PRICE, NOKUS_QUANT, NOKFH_PRICE, NOKFH_QUANT):
@@ -12,7 +12,10 @@ def ETF(NOKUS_PRICE, NOKUS_QUANT, NOKFH_PRICE, NOKFH_QUANT):
 
     if MONEY_DIFF > 15:
         # TODO: meaning NOKFH is cheaper! buy in NOKFH and sell NOKUS with the minimum quantity
-        return
+        return 1
     elif MONEY_DIFF < -15:
         # TODO: meaning NOKUS is cheaper! buy in NOKUS and sell NOKFH with the minimum quantity
-        return
+        return 0
+
+
+print(ETF(NOKUS_PRICE, NOKUS_QUANT, NOKFH_PRICE, NOKFH_QUANT))
